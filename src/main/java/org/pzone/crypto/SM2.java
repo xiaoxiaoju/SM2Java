e6e067fd728665dd8c7585167c2e200b4d6b8880
xiaoxiaoju <zjq_jobs@163.com>
2020-08-27 21:45:39 +0800

    测试项目推送

--- src/main/java/org/pzone/crypto/SM2.java
+++ src/main/java/org/pzone/crypto/SM2.java
@@ -793,16 +793,10 @@ public class SM2 {
 	public static void main(String[] args) throws UnsupportedEncodingException {
 
 		SM2 sm02 = new SM2();
-		// BigInteger px = new BigInteger(
-		// "0AE4C779 8AA0F119 471BEE11 825BE462 02BB79E2 A5844495 E97C04FF
-		// 4DF2548A".replace(" ", ""), 16);
-		// BigInteger py = new BigInteger(
-		// "7C0240F8 8F1CD4E1 6352A73C 17B7F16F 07353E53 A176D684 A9FE0C6B
-		// B798E857".replace(" ", ""), 16);
-		// ECPoint publicKey = sm02.curve.createPoint(px, py);
-		// BigInteger privateKey = new BigInteger(
-		// "128B2FA8 BD433C6C 068C8D80 3DFF7979 2A519A55 171B1B65 0C23661D
-		// 15897263".replace(" ", ""), 16);
+		 BigInteger px = new BigInteger("3E2A11321A7EDAFDDECB44ACB5A15D853F9178DEF93220AF096788C63EAE1CE6".replace(" ", ""), 16);
+		 BigInteger py = new BigInteger("B2C4BF29A2C3BC1C78E76286A68FF07ACF6946615424AEF47EBCEA5A503FB75C".replace(" ", ""), 16);
+		 ECPoint publicKey = sm02.curve.createPoint(px, py);
+		 BigInteger privateKey = new BigInteger("B55CE6F0BFD3D6E24DA599F07BDBDD1368511C67DC70165C35854722B56B8DD6".replace(" ", ""), 16);
 
 		// SM2KeyPair keyPair = sm02.generateKeyPair();
 		// ECPoint publicKey=keyPair.getPublicKey();
@@ -811,8 +805,8 @@ public class SM2 {
 		// sm02.exportPrivateKey(privateKey, "E:/privatekey.pem");
 
 		System.out.println("-----------------公钥加密与解密-----------------");
-		ECPoint publicKey = sm02.importPublicKey("E:/publickey.pem");
-		BigInteger privateKey = sm02.importPrivateKey("E:/privatekey.pem");
+//		ECPoint publicKey = sm02.importPublicKey("E:/publickey.pem");
+//		BigInteger privateKey = sm02.importPrivateKey("E:/privatekey.pem");
 		byte[] data = sm02.encrypt("测试加密aaaaaaaaaaa123aabb", publicKey);
 		System.out.print("密文:");
 		SM2.printHexString(data);
